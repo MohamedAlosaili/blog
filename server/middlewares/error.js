4b18d290f3cff0be9573e4ddcad7b8dfde82b9a0
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-03 15:49:34 +0300

    Add error handling middleware function to handle various errors and send a JSON response with appropriate status codes and messages

---
+++ server/middlewares/error.js
@@ -0,0 +1,34 @@
+const colors = require("colors");
+const ErrorResponse = require("../utils/errorResponse");
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
+  console.error(`${err.stack}`.red);
+
+  // Catch Validation errors
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors)
+      .map(err => err.message)
+      .join(", ");
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Catch duplication error
+  if (err.code === 11000) {
+    const field = Object.keys(err.keyValue)[0];
+    error = new ErrorResponse(
+      `${field} already taken. Please choose a different ${field}.`,
+      400
+    );
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: true,
+    data: null,
+    error: error.message || "Server Failed",
+  });
+};
+
+module.exports = errorHandler;
