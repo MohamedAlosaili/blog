4d68cf05a1f5662eeaed8bb7d1953cc0e029ba44
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-03 22:03:45 +0300

    Add parsing middlewares and errorhHandler, call DB connection function
    Add express body pareser
    Add cookie parser package
    Mount auth route
    Add listener on unhandledRejection to crash the server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,41 @@
 require("dotenv").config({ path: "./config/config.env" });
 
 const express = require("express");
+const connectDB = require("./config/db");
+const errorHandler = require("./middlewares/error");
+
+const colors = require("colors");
+const cookieParser = require("cookie-parser");
+
+// Route files
+const auth = require("./routes/auth");
 
 // Create express App
 const app = express();
+connectDB();
+
+// Parse Request Body
+app.use(express.json());
+
+// Parse cookie
+app.use(cookieParser());
+
+// Mount routes
+app.use("/auth", auth);
+
+// Error handler middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () =>
-  console.log(`Server is running on PORT:${PORT}`)
-);
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on PORT:${PORT}`.yellow.bold);
+  console.log(`Environment: ${process.env.NODE_ENV}`.green.bold);
+});
+
+process.on("unhandledRejection", err => {
+  console.log(`${err?.toString()} ❌`.red);
+  console.log(err?.stack);
+
+  server.close(() => process.exit(1));
+});
