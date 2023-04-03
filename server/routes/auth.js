11f810c85a020eb7aa2f23c91f3ba101e7ad139e
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-03 21:59:00 +0300

    Add authentication route
    The route is responsable of register, login, logout users
    and get the current loggedin user information

---
+++ server/routes/auth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  register,
+  login,
+  logout,
+  getCurrentUser,
+} = require("../controllers/auth");
+
+const { protect } = require("../middlewares/auth");
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout);
+router.get("/me", protect, getCurrentUser);
+
+module.exports = router;
