6a12f1269aad0479724634c87ecbcf32008dc4ef
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-04 14:33:40 +0300

    Add CRUD functionality and search feature to post routes in posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,14 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getPosts, getPost, createPost } = require("../controllers/posts");
+const {
+  getPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost,
+  searchPosts,
+} = require("../controllers/posts");
 
 const resourceType = require("../middlewares/resourceType");
 const { protect } = require("../middlewares/auth");
 
+// Redirect posts/postId/comments to comments route
+const commentRouter = require("./comments");
+router.use("/:postId/comments", commentRouter);
+
 router.route("/").get(getPosts).post(protect, createPost);
 
+router.get("/search", searchPosts);
+
 router.use("/:id", resourceType("Post"));
-router.route("/:id").get(getPost);
+router
+  .route("/:id")
+  .get(getPost)
+  .put(protect, updatePost)
+  .delete(protect, deletePost);
 
 module.exports = router;
