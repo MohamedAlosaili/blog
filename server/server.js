322a80882c7bdfffeb5623403e75679882e1baa6
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-05 15:45:06 +0300

    Add security middlewares to protect against various attacks and enable CORS for other domains to connect and make requests to the API. Update routes to include versioning in the path

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const express = require("express");
 const connectDB = require("./config/db");
 const errorHandler = require("./middlewares/error");
 
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+
 const colors = require("colors");
 const cookieParser = require("cookie-parser");
 
@@ -22,10 +29,30 @@ app.use(express.json({ limit: "30mb" }));
 // Parse cookie
 app.use(cookieParser());
 
+// # Securty middlewares #
+// Sanitize Data to prevent NoSQL injection
+app.use(mongoSanitize());
+// Helmet secure Express app by setting various HTTP security headers.
+app.use(helmet());
+// Prevent cross-site scripting (XSS) attack
+app.use(xss());
+// Limit requests per id to 60 request per period of time
+// You can limit certain routes e.g. resetpassword route
+app.use(
+  rateLimit({
+    windowMs: 5 * 60 * 1000, // 60 request limit per 5 minutes
+    max: 60,
+  })
+);
+// Protect against HTTP Parameter Pollution attacks
+app.use(hpp());
+// CORS enable other domains to connect and make requests to this API
+app.use(cors());
+
 // Mount routes
-app.use("/auth", auth);
-app.use("/posts", posts);
-app.use("/comments", comments);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/posts", posts);
+app.use("/api/v1/comments", comments);
 
 // Error handler middleware
 app.use(errorHandler);
