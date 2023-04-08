12379cc7c6c757bb826717bada99a53e964a4492
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-08 15:43:32 +0300

    Add class on html element depend on the prefer theme
    Add Home component as index route of the root route

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,13 +10,26 @@ import {
 } from "react-router-dom";
 
 import App from "./App";
+import Home, { loader as homeLoader } from "./pages/Home";
 import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />} errorElement={<ErrorPage />}></Route>
+    <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+      <Route index element={<Home />} loader={homeLoader} />
+    </Route>
   )
 );
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.className = "dark";
+} else {
+  document.documentElement.className = "light";
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
