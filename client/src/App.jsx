9d50c260929615d3af6114e0c916af4ec95b7c8d
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-09 00:28:14 +0300

    Refactor App to include UserContext & toastify container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import Header from "./components/Header";
+import { UserContextProvider } from "./UserContext";
 
-const App = () => (
-  <div>
-    <Header />
-    <Outlet />
-  </div>
-);
+const App = () => {
+  const location = useLocation();
+
+  const showHeader =
+    location.pathname !== "/login" && location.pathname !== "/signup";
+
+  return (
+    <div>
+      <UserContextProvider>
+        <ToastContainer position="top-center" />
+        {showHeader && <Header />}
+        <Outlet />
+      </UserContextProvider>
+    </div>
+  );
+};
 
 export default App;
