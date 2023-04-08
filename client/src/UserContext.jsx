6b8489029dd027471609424690688c6e42e8515d
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-09 00:29:10 +0300

    Add UserContext for users authentication

---
+++ client/src/UserContext.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { getCurrentUser } from "./fetchData";
+
+const UserContext = React.createContext();
+
+function UserContextProvider({ children }) {
+  const userState = useAuth();
+
+  return (
+    <UserContext.Provider value={userState}>{children}</UserContext.Provider>
+  );
+}
+
+function useAuth() {
+  const [user, setUser] = useState(undefined);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(undefined);
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  async function getUserInfo() {
+    !loading && setLoading(true);
+    error && setError(undefined);
+    try {
+      const user = await getCurrentUser();
+      setUser(user);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setError(err);
+      setLoading(false);
+    }
+  }
+
+  return [user, loading, error];
+}
+
+export { UserContext, UserContextProvider };
