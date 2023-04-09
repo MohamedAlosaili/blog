023ec8fff792b78bf85078c56e7ad54192b2360a
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-09 22:57:48 +0300

    Update extract login logic to separate hook
    Move login logic and controlled form handlers to separate hook
    Add signup link
    Update user state in UserContext

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,48 +1,22 @@
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Button from "../../components/Button";
 import { loginUser } from "../../fetchData";
+import { UserContext } from "../../UserContext";
 import "./login.css";
 
 const Login = () => {
+  const [user, userLoading, , getUserInfo] = useContext(UserContext);
   const location = useLocation();
-  const navigate = useNavigate();
-  const [loginInfo, setLoginInfo] = useState({ username: "", password: "" });
+  const { handleChange, login, loginInfo, logging } = useLogin(
+    location,
+    getUserInfo
+  );
 
-  useEffect(() => {
-    document.title = `<Blog /> | Login`;
-  }, []);
-
-  async function login(event) {
-    event.preventDefault();
-    const { username, password } = loginInfo;
-    if (!username || !password) {
-      toast.error("Please enter your login information");
-    }
-
-    const result = await loginUser(JSON.stringify(loginInfo));
-
-    if (!result.success) {
-      return toast.error(result.error);
-    }
-    console.log(result);
-    const navigateTo = location.state ? location.state : "/";
-    navigate(navigateTo, { replace: navigateTo });
-  }
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-
-    setLoginInfo(prevInfo => ({
-      ...prevInfo,
-      [name]: value,
-    }));
-  }
-
-  return (
+  return !user && !userLoading ? (
     <section className="login">
       <div className="login--wrapper">
         <h1 className="login--title">
@@ -71,11 +45,77 @@ const Login = () => {
               onChange={handleChange}
             />
           </label>
-          <Button>Login</Button>
+          <Button disabled={logging}>
+            {logging ? "Loggingin..." : "Login"}
+          </Button>
+          <p>
+            Don't have an account?{" "}
+            <Link
+              to="/signup"
+              className="login--signup--link"
+              state={location.state ? { from: location.state.from } : undefined}
+            >
+              Sign up
+            </Link>
+          </p>
         </form>
       </div>
     </section>
+  ) : (
+    <Navigate to="/" />
   );
 };
+
+function useLogin(location, getUserInfo) {
+  const navigate = useNavigate();
+  const [loginInfo, setLoginInfo] = useState({ username: "", password: "" });
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    document.title = `<Blog /> | Login`;
+  }, []);
+
+  async function login(event) {
+    event.preventDefault();
+    const { username, password } = loginInfo;
+
+    if (!username || !password) {
+      return toast.error("Please enter your login information");
+    }
+
+    setLoading(true);
+
+    try {
+      const result = await loginUser(JSON.stringify(loginInfo));
+
+      // Update user context
+      await getUserInfo();
+
+      setLoading(false);
+      if (!result.success) {
+        return toast.error(result.error);
+      }
+      const navigateTo = location.state ? location.state.from : "/";
+      navigate(navigateTo, { replace: navigateTo });
+
+      toast.success("Loggedin successfully", { autoClose: 1000 });
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+      toast.error(err.message);
+    }
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setLoginInfo(prevInfo => ({
+      ...prevInfo,
+      [name]: value,
+    }));
+  }
+
+  return { handleChange, login, loginInfo, logging: loading };
+}
 
 export default Login;
