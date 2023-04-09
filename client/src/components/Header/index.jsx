ff0343189de06a6748112d3b593253ffdd5fcd6e
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-09 23:09:21 +0300

    Add user menu to Header component with logout functionality

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,23 +1,67 @@
-import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
-import { BsFillSunFill, BsMoonStarsFill } from "react-icons/bs";
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { BsFillSunFill, BsMoonStarsFill, BsChevronDown } from "react-icons/bs";
+import { CgSpinnerTwo } from "react-icons/cg";
+import { FiLogOut } from "react-icons/fi";
+import { toast } from "react-toastify";
 
 import Button from "../Button";
+import { UserContext } from "../../UserContext";
+import { logoutUser } from "../../fetchData";
 import "./header.css";
 
 const Header = () => {
+  const [user, loading, error, getUserInfo] = useContext(UserContext);
   const { theme, toggleTheme } = useTheme();
   const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  async function logout() {
+    try {
+      const res = await logoutUser();
+
+      if (!res.success) toast.error(res.error);
+
+      await getUserInfo();
+
+      toast.success(res.message, { autoClose: 1000 });
+    } catch (err) {
+      toast.error(err.message);
+      console.log(err);
+    }
+  }
 
   return (
     <header className="header">
       <Link to="/" className="header--logo">
         {"<Blog />"}
       </Link>
-      <div className="header--user">
-        <Link to="/login" state={{ from: location.pathname }}>
-          <Button className="header--button">Login</Button>
-        </Link>
+      <div
+        className="header--user"
+        onClick={() => setIsMenuOpen(true)}
+        onMouseLeave={() => setIsMenuOpen(false)}
+      >
+        {loading ? (
+          <CgSpinnerTwo className="loading-spinner" />
+        ) : user ? (
+          <div className="user--info">
+            <div className="link">
+              <h3>@{user.username}</h3>
+              <BsChevronDown className="user--menu_icon" />
+            </div>
+            {isMenuOpen && (
+              <div className="user--menu">
+                <button className="menu--logout" onClick={logout}>
+                  Logout <FiLogOut strokeWidth={3} />
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <Link to="/login" state={{ from: location.pathname }}>
+            <Button className="header--button">Login</Button>
+          </Link>
+        )}
       </div>
       <button className="header--theme" onClick={toggleTheme}>
         {theme === "dark" ? <BsFillSunFill /> : <BsMoonStarsFill />}
