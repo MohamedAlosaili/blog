5ef54c36d38e84b38fa77e9e956268934f0ae4d1
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-10 17:57:33 +0300

    Update home post className

--- client/src/pages/Home/Post.jsx
+++ client/src/pages/Home/Post.jsx
@@ -1,25 +1,27 @@
 import { Link } from "react-router-dom";
 
 const Post = ({ post }) => (
-  <article className="post">
+  <article className="home--post">
     <img
       src={post.coverImage}
       alt={`${post.title} cover`}
-      className="post--img"
+      className="home--post--img"
     />
-    <div className="post--text">
+    <div className="home--post--text">
       <Link to={`posts/${post._id}`}>
-        <h1 className="post--title">{post.title}</h1>
+        <h1 className="home--post--title">{post.title}</h1>
       </Link>
-      <p className="post--date">Published On: {formatDate(post.createdAt)}</p>
+      <p className="home--post--date">
+        Published On: {formatDate(post.createdAt)}
+      </p>
       <div>
         {post.tags.map(tag => (
-          <span key={tag} className="post--tag">
+          <span key={tag} className="home--post--tag">
             {tag}
           </span>
         ))}
       </div>
-      <p className="post--summary">{post.summary}</p>
+      <p className="home--post--summary">{post.summary}</p>
       <Link to={`posts/${post._id}`} className="link">
         Read more
       </Link>
