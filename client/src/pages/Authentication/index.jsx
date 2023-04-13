c22a2a2f4b7418ece74cd33fc98adbeffde397c3
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-14 00:56:20 +0300

    Remove explicit return & pass page links as prop

--- client/src/pages/Authentication/index.jsx
+++ client/src/pages/Authentication/index.jsx
@@ -8,37 +8,29 @@ const Authentication = ({
   loading,
   onSubmit,
   actionButton,
-  location,
-}) => {
-  return (
-    <section className="auth">
-      <div className="auth--wrapper">
-        <h1 className="auth--title">
-          {page} to <span>{"<Blog />"}</span>
-        </h1>
-        <form className="auth--form" onSubmit={onSubmit}>
-          {/* children Form inputs */}
-          {children}
-          <button disabled={loading} className="blog--btn">
-            {actionButton}
-          </button>
-          <p>
-            {page === "Login"
-              ? "Don't have an account?"
-              : "Already have an account?"}{" "}
-            <Link
-              to={`/${page === "login" ? "signup" : "login"}`}
-              className="auth--link"
-              state={location.state ? { from: location.state.from } : undefined}
-              replace={`/${page === "login" ? "signup" : "login"}`}
-            >
-              {page === "login" ? "Sign up" : "Login"}
-            </Link>
-          </p>
-        </form>
-      </div>
-    </section>
-  );
-};
+  links,
+}) => (
+  <section className="auth">
+    <div className="auth--wrapper">
+      <h1 className="auth--title">
+        {page.replace ? (
+          page.name
+        ) : (
+          <>
+            {page.name} to <span>{"<Blog />"}</span>
+          </>
+        )}
+      </h1>
+      <form className="auth--form" onSubmit={onSubmit}>
+        {/* children Form inputs */}
+        {children}
+        <button disabled={loading} className="blog--btn">
+          {actionButton}
+        </button>
+        {links}
+      </form>
+    </div>
+  </section>
+);
 
 export default Authentication;
