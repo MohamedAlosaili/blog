7a7144024057cd7fa3a3898ab954ea176bb54810
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-04-14 14:36:37 +0300

    Refactor API_BASE to use a hardcoded URL in fetchData.js

--- client/src/fetchData.js
+++ client/src/fetchData.js
@@ -1,4 +1,5 @@
-const API_BASE = import.meta.env.VITE_API_URL;
+// const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = "https://b-log.herokuapp.com";
 
 const request = async (method, url, body) => {
   const options = { method, credentials: "include" };
